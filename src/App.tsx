21e1fa0ad638ff4c4220ef4cc82046e9e408959a
mustazir <billah15-5193@diu.edu.bd>
2025-07-26 22:00:52 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+       <div className="flex min-h-svh ">
+      <Button>Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
+
+
