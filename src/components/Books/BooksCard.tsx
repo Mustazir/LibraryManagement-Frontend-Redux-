180f4965aefe5deda19011193f9e2167d3c9c991
mustazir <billah15-5193@diu.edu.bd>
2025-07-29 22:33:10 +0600

    axios

---
+++ src/components/Books/BooksCard.tsx
@@ -0,0 +1,32 @@
+/*
+_id:686fc84853f1528a12400e1d
+title:"Becoming Steve Jobs"
+author:"Brent Schlender"
+genre:"BIOGRAPHY"
+isbn:"BIOGRAPHY5432109"
+description:"Biography of the Apple co-founder."
+copies:1
+available:true
+createdAt:2025-07-10T14:03:52.352+00:00
+updatedAt:2025-07-10T14:03:52.352+00:00
+
+
+{
+  "_id": {
+    "$oid": "686fd265a070a49994eb8927"
+  },
+  "book": {
+    "$oid": "686fc85953f1528a12400e27"
+  },
+  "quantity": 12,
+  "dueDate": {
+    "$date": "2025-07-18T00:00:00.000Z"
+  },
+  "createdAt": {
+    "$date": "2025-07-10T14:47:01.560Z"
+  },
+  "updatedAt": {
+    "$date": "2025-07-10T14:47:01.560Z"
+  }
+}
+*/
